2a35bc7fcc145325f33289c6b4c964792216158f
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-12-19 23:20:15 +0100

    feat: implemented a basic integer property

--- configuration/Property.py
+++ configuration/Property.py
@@ -7,12 +7,11 @@ class Property(Element):
     _value: Any
     _value_type = Any
 
-    def __init__(self, parent: Element|None, name: str, value: _value_type = None):
+    def __init__(self, parent: Element|None, name: str, value: _value_type|None = None):
         super().__init__(parent, name)
         self.value = value
 
-    @staticmethod
-    def is_value_valid(value: Any) -> bool:
+    def is_value_valid(self, value: _value_type) -> bool:
         return True
 
     @property
@@ -32,7 +31,32 @@ class Property(Element):
     def __str__(self) -> str:
         return f'{self.__class__.__name__}<{self._value_type.__name__}> {self.name}({repr(self.value)})'
 
+class IntProperty(Property):
+    _value_type = int
+    _min: int|None
+    _max: int|None
 
+    def __init__(self, parent: Element|None, name: str, value: _value_type|None = None, min: int|None = None, max: int|None = None):
+        self._min = min
+        self._max = max
+        super().__init__(parent, name, value)
+
+    @property
+    def min(self):
+        return self._min
+
+    @property
+    def max(self):
+        return self._max
+
+    def is_value_valid(self, value: _value_type) -> bool:
+        if self._min is not None and value < self._min:
+            return False
+
+        if self._max is not None and value > self._max:
+            return False
+
+        return True
 
 if __name__ == '__main__':
     prop = Property(None, "myProp")
@@ -49,5 +73,20 @@ if __name__ == '__main__':
         print(prop)
     except Exception as e:
         print(e.__class__.__name__, e)
-    
+
     print(Property(prop, "lambda"))
+
+    int_prop = IntProperty(None, "intProp", 879, min=0)
+    print(int_prop)
+
+    try:
+        int_prop.value = 3.14
+        print(int_prop)
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+    try:
+        int_prop.value = -1
+        print(int_prop)
+    except Exception as e:
+        print(e.__class__.__name__, e)
