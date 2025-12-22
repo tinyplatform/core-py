f17f622d85f03d58b366f9692bf22173e3678bd1
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-12-22 22:52:30 +0100

    thing

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
+from PySide6.QtCore import QSize, Qt, QDir
 from Application import *
 
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QWidget, QMainWindow
+from PySide6.QtWidgets import QTabWidget, QWidget, QMainWindow, QTreeView, QVBoxLayout, QFileSystemModel
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        treeModel = QFileSystemModel()
+        treeModel.setRootPath(QDir.currentPath())
+
+        treeView = QTreeView()
+        treeView.setModel(treeModel)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(treeView)
+        self.setLayout(layout)
 
 class TestApp(Application):
     app_ID = 'test'
@@ -12,10 +27,7 @@ class TestApp(Application):
         print('NEW APP INSTANCE')
         print(self.app_ID, self.configuration_version, self.app_version)
 
-        self.mainWindow = QMainWindow()
-        menuBar = self.mainWindow.menuBar()
-        file = menuBar.addMenu("&File")
-
+        self.mainWindow = MyWindow()
         self.mainWindow.show()
 
 app = TestApp()
